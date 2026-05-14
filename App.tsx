08ef8909b479cc4ee06dee7c6fba597367489ab3
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2026-05-14 10:06:22 +0300

    Updated npm cache

--- App.tsx
+++ App.tsx
@@ -11,6 +11,11 @@ import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { BaseIcon, BaseIconName, hideBootSplash } from '@/shared';
 
+import { createMMKV } from 'react-native-mmkv';
+const storage = createMMKV();
+
+// add this line inside your App.tsx
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
