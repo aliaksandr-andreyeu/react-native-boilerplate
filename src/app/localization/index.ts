029d61abf2eccd267e22729d8434e72e4c1175db
Aliaksandr Andreyeu <andreyeu.aliaksandr@gmail.com>
2026-05-19 23:53:24 +0300

    Fixed tests

--- src/app/localization/index.ts
+++ src/app/localization/index.ts
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { getLocales } from 'react-native-localize';
-import { getAppLanguage, setAppLanguage } from '@/shared/utils/storage';
+import { getAppLanguage, setAppLanguage } from '@/shared/lib/storage';
 
 import en from './locales/en.json';
 /*
